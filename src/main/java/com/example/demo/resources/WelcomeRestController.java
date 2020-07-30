2a76a2fa26ec7eec6e00c84764008fec22bac9fb
Ashok IT <30829130+Ashok-IT-School@users.noreply.github.com>
2020-07-31 02:06:16 +0530

    Update WelcomeRestController.java

--- src/main/java/com/example/demo/resources/WelcomeRestController.java
+++ src/main/java/com/example/demo/resources/WelcomeRestController.java
@@ -1,7 +1,7 @@
 package com.example.demo.resources;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.annotations.Api;
