fd6f00d0d6ed99e95d566088fb6e72a2eeb78078
Ashok IT <30829130+Ashok-IT-School@users.noreply.github.com>
2020-07-31 18:42:28 +0530

    Update WelcomeRestController.java

--- src/main/java/com/example/demo/resources/WelcomeRestController.java
+++ src/main/java/com/example/demo/resources/WelcomeRestController.java
@@ -14,7 +14,7 @@ public class WelcomeRestController {
 	@ApiOperation(value = "Get Welcome Message For The Given Name ", response = String.class, tags = "getWelcomeNote")
 	@GetMapping("/welcome/{name}")
 	public String welcome(@PathVariable("name") String name) {
-		return name+", Welcome to Ashok IT Family..!!";
+		return name+", Welcome to Ashok IT..!!";
 	}
 
 }
